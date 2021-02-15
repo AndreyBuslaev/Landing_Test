780a56002721f90387bab731eda030d434227f2c
AndreyBuslaev <AndreyBuslaev@users.noreply.github.com>
2021-02-16 00:21:48 +0200

    add form dynamic

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,10 @@ const form = document.querySelector('#formData');
 form.onsubmit = async (e) => {
     e.preventDefault();
 
-    let response = await fetch('http://localhost/12344', {
+    let response = await fetch('http://localhost', {
       method: 'POST',
       body: new FormData(form),
+      'Content-Type': 'application/x-www-form-urlencoded'
     });
 
     let result = await response.json();
